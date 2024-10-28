3d83d5e9a0eb0303ee8c2fe2ce4a8701b0102245
Kshitij Kumar <kshitijyadav2003@gmail.com>
2024-10-29 04:42:22 +0530

    prisma

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,5 @@
+import { PrismaClient } from "@repo/db/client"
+const client = new PrismaClient();
 export default function Page(): JSX.Element {
   return (
     <div className="bg-red-800 text-2xl" >
